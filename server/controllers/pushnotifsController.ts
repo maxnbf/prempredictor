569a814637cf7f1fce554436d00c2f1ed5012658
Max Breslauer-Friedman <maxnbf@Maxs-MacBook-Pro-3.local>
2025-11-28 16:29:13 -0500

    Adds log for saving notification token, for debugging purposes

--- server/controllers/pushnotifsController.ts
+++ server/controllers/pushnotifsController.ts
@@ -3,6 +3,8 @@ import * as pushnotifService from "../services/pushNotificationService"
 export async function saveToken(request) {
     const userId = request?.body?.user?.userId;
     const { token } = request.body;
+
+    console.log("Saving token for ", userId, + " at ", new Date().toISOString())
     
     return await pushnotifService.saveToken(token, userId)
 }
