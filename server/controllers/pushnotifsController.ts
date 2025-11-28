2464fe3a907c3403129237e8de4387e47656bee5
Max Breslauer-Friedman <maxnbf@Maxs-MacBook-Pro-3.local>
2025-11-28 16:31:56 -0500

    log formatting

--- server/controllers/pushnotifsController.ts
+++ server/controllers/pushnotifsController.ts
@@ -4,7 +4,7 @@ export async function saveToken(request) {
     const userId = request?.body?.user?.userId;
     const { token } = request.body;
 
-    console.log("Saving token for ", userId, + " at ", new Date().toISOString())
+    console.log("Saving token for " + userId + " at " + new Date().toISOString())
     
     return await pushnotifService.saveToken(token, userId)
 }
