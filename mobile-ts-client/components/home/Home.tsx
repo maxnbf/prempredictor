e9bc7d97517561885f08142840fda46baf29aa36
Max Breslauer-Friedman <maxnbf@Maxs-MacBook-Pro-3.local>
2025-11-28 19:32:35 -0500

    reports notif status granted

--- mobile-ts-client/components/home/Home.tsx
+++ mobile-ts-client/components/home/Home.tsx
@@ -92,6 +92,7 @@ export const Home = () => {
 
       if (finalStatus !== "granted") {
         //alert("Failed to get push token for push notification!");
+        await failedNotif(finalStatus)
         return;
       }
 
@@ -99,7 +100,7 @@ export const Home = () => {
         token = (await Notifications.getExpoPushTokenAsync({ projectId: Constants.expoConfig?.extra?.eas.projectId })).data;
       } catch (e) {
         if (e instanceof Error) {
-          failedNotif(e.message)
+          await failedNotif(e.message)
         }
       }
     } else {
